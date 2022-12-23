4a62dbc1fa55690f75d99d2c8865863adfdd9a63
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2022-12-23 13:26:09 -0500

    better tile state change

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
 import React, { FC, useState } from 'react';
 import "./index.css";
-
+import { isPropertySignature } from 'typescript';
+import { setUncaughtExceptionCaptureCallback } from 'process';
 
 interface TileProps {
   pieceId: string;
   i: number;
   j: number;
+  clickTile: Function;
 }
 
 const Tile: FC<TileProps> = (props) => {
+  const [pieceId, setPieceId] = useState(props.pieceId);
+
+  function handleClick() {
+    console.log("handleClick()");
+    let res: number = props.clickTile(props.i, props.j);
+    console.log(res);
+
+    setPieceId("ZZZ");
+  }
+
   return (
-    <button className="tile">
-      {props.pieceId}
+    <button className="tile" onClick={() => handleClick()}>
+      {pieceId}
     </button>
   )
 }
@@ -19,30 +31,33 @@ const Tile: FC<TileProps> = (props) => {
 interface RowProps {
   row: string[];
   i: number;
+  clickTile: Function; 
 }
 
 const Row: FC<RowProps> = (props) => {
   return (
     <div className="board-row">
-      <tr>
-        {props.row.map((pieceId, j) => <Tile pieceId={pieceId} i={props.i} j={j}/> )}
-      </tr>
+      {props.row.map((pieceId, j) => <Tile pieceId={pieceId} i={props.i} j={j} clickTile={props.clickTile}/> )}
     </div>
   )
 }
 
 const Board: FC = () => {
-  let startBoard = [
+  const board = [
     ["a", "b", "c", "*"],
-    ["d", "e", "f", "*"]
+    ["d", "e", "f", "king-black"]
   ];
-  const [curBoard, setCurBoard] = React.useState(startBoard);
+
+  function clickTile(i: number, j: number): number {
+    console.log(i + "-" + j);
+    return 42;
+  }
 
   return (
     <div>
       <h1>Chess</h1>
       <div>
-        {curBoard.map((row, i) => <Row row={row} i={i}/> )}
+        {board.map((row, i) => <Row row={row} i={i} clickTile={clickTile}/> )}
       </div>
     </div>
   );
