e4f5d2cdaaa97790714b1f1ba75ab855cb26cd80
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2023-01-04 09:48:47 -0500

    clean slate board

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
-import React, { useState } from "react";
+import React, { FC, useState, useRef } from "react";
+import "./index.css";
+
+
+interface BoardProps {
+  board: string[][];
+  clickTile: Function;
+}
+const Board: FC<BoardProps> = (props) => {
+  return (
+    <div className="board">
+      {props.board.map((row, r) =>
+        <div className="board-row" key={r}>
+          {row.map((pieceId, c) =>
+            <div className="board-row-tile" 
+            onClick={() => props.clickTile(r, c)} key={c}>
+              <img className="board-row-tile-img" 
+              src={require(`./images/${pieceId}.png`)}/>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
 
 function App() {
   const [board, setBoard] = useState<string[][]>([
@@ -12,9 +37,13 @@ function App() {
       ["rl", "nl", "bl", "ql", "kl", "bl", "nl", "rl"]
   ]);
 
+  function clickTile(r: number, c: number) {
+    console.log(r, c);
+  }
+
   return (
-    <div>
-        Hello.
+    <div className="flex-container">
+      <Board board={board} clickTile={clickTile}/>
     </div>
   );
 }
